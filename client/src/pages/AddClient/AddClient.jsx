5eb929fed0a4ba8f829d0ba6ddc734ea31288d34
Luiz Omena <83364189+luizfilipeyep@users.noreply.github.com>
2024-10-15 10:53:44 -0300

    [client] front AddClient

--- client/src/pages/AddClient/AddClient.jsx
+++ client/src/pages/AddClient/AddClient.jsx
@@ -1,9 +1,32 @@
-function addClient() {
+import "./addclient.css"
+
+function AddClient() {
   return ( 
     <>
-      
+      <div className="AddClientWrapper">
+        <h1>Cadastro de Clientes</h1>
+        <p>Preencha os campos abaixo para salvar o cliente:</p>
+        <div className="inputWrapper">
+          <label htmlFor="name">Nome: </label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div className="inputWrapper">
+          <label htmlFor="age">Idade: </label>
+          <input type="text" name="age" id="age" />
+        </div>
+        <div className="inputWrapper">
+          <label htmlFor="uf">UF: </label>
+          <select name="uf" id="">
+            <option value="AC">AC</option>
+            <option value="AL">AL</option>
+          </select>
+        </div>
+        <div className="inputWrapper">
+          <a href="#">Cancelar</a> | <button>Salvar</button>
+        </div>
+      </div>
     </>
    );
 }
 
-export default addClient;
+export default AddClient;
