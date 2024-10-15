3e830ec4994978bff0ff97abb6c9365cb01183b7
Luiz Omena <83364189+luizfilipeyep@users.noreply.github.com>
2024-10-15 11:19:13 -0300

    [server] post /add/client

--- server/index.js
+++ server/index.js
@@ -13,6 +13,19 @@ const db = mysql.createPool({
 app.use(cors())
 app.use(express.json())
 
+app.post("/add/client", (req, res) => {
+  const name = req.body.name
+  const age = req.body.age
+  const uf = req.body.uf
+
+  let SQL = "INSERT INTO cliente ( name, age, uf ) VALUES ( ?, ?, ? )"
+
+  db.query(SQL, [name, age, uf], (err, result) => {
+    if (err) res.send({ msg: err })
+    else res.send({ msg: "Usuário Cadastrado" })
+  })
+})
+
 app.listen(8800, () => {
   console.log("servidor iniciado");
 })
