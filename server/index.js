60869bd4860b8e6f9e7e77b15e97d40688286796
luizfilipeyep <83364189+luizfilipeyep@users.noreply.github.com>
2024-10-16 17:22:16 -0300

    [server] CRUD cliente

--- server/index.js
+++ server/index.js
@@ -13,16 +13,50 @@ const db = mysql.createPool({
 app.use(cors())
 app.use(express.json())
 
+app.get("/get/client", (req, res) => {
+  let SQL = "SELECT * FROM cliente;"
+
+  db.query(SQL, (err, result) => {
+    if (err) res.send({ msg: err })
+    else res.send(result)
+  })
+})
+
 app.post("/add/client", (req, res) => {
   const name = req.body.name
   const age = req.body.age
   const uf = req.body.uf
 
-  let SQL = "INSERT INTO cliente ( name, age, uf ) VALUES ( ?, ?, ? )"
+  let SQL = "INSERT INTO cliente ( name, age, uf ) VALUES ( ?, ?, ? );"
 
   db.query(SQL, [name, age, uf], (err, result) => {
     if (err) res.send({ msg: err })
-    else res.send({ msg: "Usuário Cadastrado" })
+    else res.send({ msg: "Usuário Cadastrado!" })
+  })
+})
+
+app.put("/edit/client", (req, res) => {
+  const cod_cliente = req.body.cod_cliente
+  const name = req.body.name
+  const age = req.body.age
+  const uf = req.body.uf
+
+  let SQL = "UPDATE cliente SET name = ?, age = ?, uf = ? WHERE cod_cliente = ?;"
+
+  db.query(SQL, [name, age, uf, cod_cliente], (err, result) => {
+    if (err) res.send({ msg: err })
+    else res.send({ msg: "Edição feita!" })
+  })
+})
+
+app.delete("/delete/client", (req, res) => {
+  const cod_cliente = req.body.cod_cliente
+
+  let SQL = "DELETE FROM cliente WHERE cod_cliente = ?;"
+
+  db.query(SQL, [cod_cliente], (err, result) => {
+    if (err) res.send({ msg: err })
+    else res.send({ msg: "Apagado!" })
   })
 })
 
